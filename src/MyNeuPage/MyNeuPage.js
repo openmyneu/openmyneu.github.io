6835b50ca247b2e12f13930f5f1d3bd32695ef71
Kevin Johnson <kevin@coolkev.com>
2017-12-17 03:44:16 -0500

    Did some preliminary dropdown hoverbar stuff, need to check to see if it works on phones

--- src/MyNeuPage/MyNeuPage.js
+++ src/MyNeuPage/MyNeuPage.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
+import './dropdownhoverbar.css'
+
 export default function MyNeuPage() {
   return (
-    <div>
-      MyNeuPage
+    <div className="contentPage myNeuPage">
+      <div className="hoverBar">
+
+        <div className="dropdown">
+          <button>Internet</button>
+          <div>
+            <a href="https://www.google.com">Google</a>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://my.northeastern.edu">Real myNortheastern</a>
+            <a href="https://www.twitter.com">Twitter</a>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button>Finances</button>
+          <div>
+            <a href="https://www.google.com">FASFA</a>
+            <a href="https://www.facebook.com">FASFA</a>
+            <a href="https://my.northeastern.edu">FASFA</a>
+            <a href="https://www.twitter.com">FASFA</a>
+          </div>
+        </div>
+
+        <a className="dropdownBarDirectLink" href="https://blackboard.neu.edu">Blackboard</a>
+
+      </div>
     </div>
   )
 }
+
+/*
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2sdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+ */
